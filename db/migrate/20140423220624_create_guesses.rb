86b68de23fb64ad069b8a6ef1079bf2c0754af29
Priyanka Rathi <priyankajmantri@gmail.com>
2014-04-30 10:02:19 +1200

    update migration

--- db/migrate/20140423220624_create_guesses.rb
+++ db/migrate/20140423220624_create_guesses.rb
@@ -1,13 +1,13 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
-      t.integer :game_id , :null => false
+      t.integer :mastermind_game_id , :null => false
       t.string :first_color , :null => false
       t.string :second_color , :null => false
       t.string :third_color , :null => false
       t.string :fourth_color , :null => false
       t.timestamps
-      t.index :game_id
+      t.index :mastermind_game_id
     end
   end
 end
--- db/migrate/20140427072549_alter_column_target_guess_number_of_game.rb
+++ db/migrate/20140427072549_alter_column_target_guess_number_of_game.rb
@@ -1,5 +1,5 @@
 class AlterColumnTargetGuessNumberOfGame < ActiveRecord::Migration
   def change
-    change_column :games , :target_guess_number , :string
+    change_column :games , :target_guess_number , :string , :default => ""
   end
 end
