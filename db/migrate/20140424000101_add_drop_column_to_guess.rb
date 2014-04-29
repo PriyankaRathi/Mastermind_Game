eaff248d4e13f4e97b2c3eeee0ac28f47a165903
Priyanka Rathi <priyankajmantri@gmail.com>
2014-04-30 11:19:55 +1200

    changed migration scripts

--- db/migrate/20140424000101_add_drop_column_to_guess.rb
+++ db/migrate/20140424000101_add_drop_column_to_guess.rb
@@ -1,6 +1,7 @@
 class AddDropColumnToGuess < ActiveRecord::Migration
   def change
-    add_column :guesses, :game_id, :integer , :null => false
+    add_column :guesses, :game_id, :integer
+    change_column :guesses, :game_id, :integer , :null => false
     add_index :guesses, :game_id
     remove_column :guesses, :mastermind_game_id
   end
